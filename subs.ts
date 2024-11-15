1b9490f74772a022be166021f957edf7e7d2c0cd
yousonnet <orionxiaohan@gmail.com>
2024-11-15 19:26:24 +0800

    scaling samll

--- subs.ts
+++ subs.ts
@@ -7,8 +7,7 @@ import Client, { SubscribeRequest, CommitmentLevel } from "@triton-one/yellowsto
 const token:string = process.env.token!;
 const endpoint:string = process.env.endpoint!;
 console.log(token,endpoint)
-const client = new Client(endpoint, token, { "grpc.max_receive_message_length": 64 * 1024 * 1024,"grpc.max_concurrent_streams": 1024,"grpc.default_compression_algorithm": 2
-
+const client = new Client(endpoint, token, { "grpc.max_receive_message_length": 8 * 1024 * 1024,
  });
 
 async function test() {
